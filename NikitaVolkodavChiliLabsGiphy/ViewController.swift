ec74528dbd34020b08fff26d922b352efdc541e8
NikitaVolkodav <nikita15.201252@gmail.com>
2023-11-29 13:44:46 +0200

    git-innit

---
+++ NikitaVolkodavChiliLabsGiphy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NikitaVolkodavChiliLabsGiphy
+//
+//  Created by Nikita Volkodav on 29.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
