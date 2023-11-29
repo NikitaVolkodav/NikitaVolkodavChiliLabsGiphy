84359aa82119851f0cd07037805535f3f65ca01d
NikitaVolkodav <nikita15.201252@gmail.com>
2023-11-29 13:53:36 +0200

    change files, removed main

---
+++ NikitaVolkodavChiliLabsGiphy/SuportingFiles/SceneDelegate.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+        var window: UIWindow?
+
+        func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+            guard let windowScene = (scene as? UIWindowScene) else { return }
+            let window = UIWindow(windowScene: windowScene)
+            let mainViewController = ViewController()
+            window.rootViewController = mainViewController
+            window.makeKeyAndVisible()
+            self.window = window
+        }
+
+}
+
--- NikitaVolkodavChiliLabsGiphy/ViewController.swift
+++ NikitaVolkodavChiliLabsGiphy/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  NikitaVolkodavChiliLabsGiphy
-//
-//  Created by Nikita Volkodav on 29.11.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
